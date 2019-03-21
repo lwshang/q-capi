580ed8f1913e056af397b30cea2ba548fa620fba
lwshang <lwsh37@gmail.com>
2019-03-21 17:08:22 -0400

    Use emutls.c.o to build libkdb on macOS
    emutls.c.o is extracted from libclang_rt provided by Xcode/commandline tools

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 extern crate cc;
 
 use std::env;
-use std::path::PathBuf;
-use std::process::Command;
 
 fn main() {
     let host = env::var("HOST").unwrap();
@@ -14,19 +12,12 @@ fn main() {
 
     if cfg!(feature = "ipc") {
         println!("cargo:rustc-link-lib=static=kdb");
-        
+
         #[cfg(target_os = "macos")]
         cc::Build::new()
             .object("src/kdb/m64/e.o")
+            .object("src/kdb/m64/emutls.c.o")
             .compile("libkdb.a");
-        let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-        Command::new("lipo")
-            .arg("/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/10.0.0/lib/darwin/libclang_rt.osx.a")
-            .arg("-thin").arg("x86_64").arg("-output")
-            .arg(dst.join("libclang_rt.osx.a"))
-            .output()
-            .expect("failed to extract thin library");
-        println!("cargo:rustc-link-lib=static=clang_rt.osx");
 
         #[cfg(target_os = "linux")]
         cc::Build::new()
