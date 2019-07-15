691a1cdae6c438b8efccd0e50b8455a41cb6b62d
lwshang <lwsh37@gmail.com>
2019-07-15 18:07:47 -0400

    Turn off some ctests
    * roundtrip
    * V (void)

--- systest/build.rs
+++ systest/build.rs
@@ -10,7 +10,7 @@ fn main() {
     // Include the directory where the header files are defined
     cfg.include("src");
 
-    // Skip some tests 
+    // Skip some tests
     cfg.skip_struct(|ty| {
         match ty {
             "U" => true, // TODO: figure out how to test opauqe struct type U
@@ -18,22 +18,26 @@ fn main() {
         }
     });
 
+    cfg.skip_type(|s| match s {
+        "V" => true,
+        _ => false,
+    });
+
     // following functions are not defined in e.o, so skip here
-    cfg.skip_fn(|x| {
-        match x {
-            "dl" | "dot" | "sd0" | "sd0x" | "sd1" => true,
-            _ => false,
-        }
+    cfg.skip_fn(|x| match x {
+        "dl" | "dot" | "sd0" | "sd0x" | "sd1" => true,
+        _ => false,
     });
 
     // Translate some Rust types to C
-    cfg.type_name(|ty, _, _| {
-    match ty {
+    cfg.type_name(|ty, _, _| match ty {
         "Sconst" => "S".to_string(),
         "k0" => "struct k0".to_string(),
         _ => ty.to_string(),
-    }
-});
+    });
+
+    // Skip roundtrip tests
+    cfg.skip_roundtrip(|_| true);
 
     // Generate the tests, passing the path to the `*-sys` library as well as
     // the module to generate.
